56621ce1af2040669b0ae887d41fdcd5f15dca10
Brett Richter <brettrichter@nypl.org>
2022-08-22 16:19:08 -0400

    trying a stack with alb

---
+++ lamp_ec2_alb/elb.tf
@@ -0,0 +1,48 @@
+resource "aws_lb_target_group" "instance-tg" {
+  name        = "tf-example-lb-instance-tg"
+  target_type = "instance"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = "vpc-04ac4a3b4e0551328"
+}
+
+# Create a new load balancer
+resource "aws_alb" "alb" {
+  name               = "test-terraform-alb"
+  
+  subnets = [
+              "subnet-011400a675d566141",
+              "subnet-0071ac6d55c431e43"
+  ]
+  security_groups = ["${aws_security_group.ec2-alb.id}"]
+
+
+  tags = {
+    Name = "test-terraform-elb"
+  }
+}
+
+resource "aws_lb_listener" "alb80" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = "arn:aws:acm:us-east-1:685731035297:certificate/db3e055b-2182-4fc7-a735-8e8b1b987bf6"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.instance-tg.arn
+  }
+}
+resource "aws_lb_listener" "alb443" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = "arn:aws:acm:us-east-1:685731035297:certificate/db3e055b-2182-4fc7-a735-8e8b1b987bf6"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.instance-tg.arn
+  }
+}
---
+++ lamp_ec2_alb/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "nypl-dams-dev"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server_alb" {
+  ami           = "ami-06a112af5bb8c0acb"
+  instance_type = "t3.micro"
+  subnet_id = "subnet-0b2baaeaa3a74d851"
+  key_name = "nypl_sa2"
+  vpc_security_group_ids = ["${aws_security_group.ec2.id}"]
+
+  tags = {
+    Name = "nginx_web_server_alb"
+    Project = "TestProject"
+  }
+}
