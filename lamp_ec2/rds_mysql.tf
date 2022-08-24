16e514b7e34aedef752f672c6d0857e6c3dc9691
Brett Richter <brettrichter@nypl.org>
2022-08-24 11:32:32 -0400

    updating rds to use correct param grp, and commiting state files

--- lamp_ec2/rds_mysql.tf
+++ lamp_ec2/rds_mysql.tf
@@ -19,7 +19,7 @@ resource "aws_db_instance" "default" {
   db_name              = "terraform_mydb"
   username             = "foo"
   password             = "foobarbaz"
-  parameter_group_name = "defaultmysql5.7"
+  parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   vpc_security_group_ids = ["${aws_security_group.rds_terraform.id}"]
   db_subnet_group_name = "${aws_db_subnet_group.default.id}"
