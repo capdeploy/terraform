a33a4593639da7d11ac941255664f9e845249f98
Brett Richter <brettrichter@nypl.org>
2022-09-09 13:43:33 -0400

    saving shell

--- es/es.sh
+++ es/es.sh
@@ -8,8 +8,7 @@ wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.16.1-l
 
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.16.1-linux-x86_64.tar.gz.sha512
 shasum -a 512 -c elasticsearch-7.16.1-linux-x86_64.tar.gz.sha512
-cd /opt
-tar xvfz ~/elasticsearch-7.16.1-linux-x86_64.tar.gz
+cd /opt && tar xvfz ~/elasticsearch-7.16.1-linux-x86_64.tar.gz
 
 groupadd elasticsearch && useradd elasticsearch -g elasticsearch -p elasticsearch
 
@@ -18,3 +17,6 @@ chown -R elasticsearch:elasticsearch elasticsearch && chown -R elasticsearch:ela
 ln -s elasticsearch-7.16.1 elasticsearch
 sysctl -w vm.max_map_count=262144
 su - elasticsearch
+
+edit /ets/hosts
+edit elasticsearch/config/elasticsearch.yml
