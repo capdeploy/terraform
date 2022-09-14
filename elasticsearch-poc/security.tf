471130bc29147d8e41e7fe8b54faca045ce7fb14
Brett Richter <brettrichter@nypl.org>
2022-09-14 14:29:31 -0400

    making some changes to the elasticsearch poc before merging into main nypl repo

---
+++ elasticsearch-poc/security.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "ec2" {
+
+  name        = var.ec2_security_group
+  description = "Allow SSH ES Ports"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+    ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+    ingress {
+    from_port   = 9300
+    to_port     = 9300
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ elasticsearch-poc/var.tf
@@ -0,0 +1,49 @@
+variable "vpc_id" {
+  type = string
+  default = "vpc-04ac4a3b4e0551328"
+}
+
+variable "key_name" {
+  type = string
+  default = "nypl_sa2"
+}
+
+variable "aws_profile" {
+  type = string
+  default = "nypl-dams-dev"
+}
+
+variable "ec2_ami" {
+  type = string
+  default = "ami-09a41e26df464c548"
+}
+
+variable "ec2_instance_type" {
+  type = string
+  default = "t3.small"
+}
+
+variable "ebs_vol_size" {
+  type = string
+  default = "50"
+}
+
+variable "tag_name" {
+  type = string
+  default = "brett-test-es"
+}
+
+variable "tag_project" {
+  type = string
+  default = "DevOps"
+}
+
+variable "tag_role" {
+  type = string
+  default = "DEVOPS-ES"
+}
+
+variable "ec2_security_group" {
+  type = string
+  default = "test terraform ES grp"
+}
