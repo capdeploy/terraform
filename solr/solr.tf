ecaa8a79a79057557ea517190b16858707eb4ffb
Brett Richter <brettrichter@nypl.org>
2022-09-21 10:48:42 -0400

    first step to combining zk and sc

---
+++ solr/solr.tf
@@ -0,0 +1,58 @@
+resource "aws_instance" "sc_server1" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type_sc
+  subnet_id = "subnet-0b2baaeaa3a74d851"
+  key_name = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.ec2.id}"]
+  
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = var.ebs_vol_size_sc
+  }
+
+  tags = {
+    Name = var.tag_name_sc
+    Project = var.tag_project
+    Role = var.tag_role_sc
+  }
+}
+
+resource "aws_instance" "sc_server2" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type_sc
+  subnet_id = "subnet-0b2baaeaa3a74d851"
+  key_name = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.ec2.id}"]
+
+    ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = var.ebs_vol_size_sc
+  }
+
+
+  tags = {
+    Name = var.tag_name_sc
+    Project = var.tag_project
+    Role = var.tag_role_sc
+  }
+}
+
+resource "aws_instance" "sc_server3" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type_sc
+  subnet_id = "subnet-0b2baaeaa3a74d851"
+  key_name = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.ec2.id}"]
+
+    ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = var.ebs_vol_size_sc
+  }
+
+
+  tags = {
+    Name = var.tag_name_sc
+    Project = var.tag_project
+    Role = var.tag_role_sc
+  }
+}
