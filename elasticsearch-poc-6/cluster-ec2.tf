3c2f38c593a94e380e13aff8359c3284114dd4cc
Brett Richter <brettrichter@nypl.org>
2023-03-01 15:38:33 -0500

    working on es 6

---
+++ elasticsearch-poc-6/cluster-ec2.tf
@@ -0,0 +1,61 @@
+resource "aws_instance" "es_server1" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  subnet_id = "subnet-0b2baaeaa3a74d851"
+  key_name = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.ec2.id}"]
+  
+  ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = var.ebs_vol_size
+  }
+
+  tags = {
+    Name = var.tag_name
+    Project = var.tag_project
+    Role = var.tag_role
+    InstanceID = 4
+  }
+}
+
+resource "aws_instance" "es_server2" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  subnet_id = "subnet-0b2baaeaa3a74d851"
+  key_name = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.ec2.id}"]
+
+    ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = var.ebs_vol_size
+  }
+
+
+  tags = {
+    Name = var.tag_name
+    Project = var.tag_project
+    Role = var.tag_role
+    InstanceID = 5
+  }
+}
+
+resource "aws_instance" "es_server3" {
+  ami           = var.ec2_ami
+  instance_type = var.ec2_instance_type
+  subnet_id = "subnet-0b2baaeaa3a74d851"
+  key_name = var.key_name
+  vpc_security_group_ids = ["${aws_security_group.ec2.id}"]
+
+    ebs_block_device {
+    device_name = "/dev/xvda"
+    volume_size = var.ebs_vol_size
+  }
+
+
+  tags = {
+    Name = var.tag_name
+    Project = var.tag_project
+    Role = var.tag_role
+    InstanceID = 6
+  }
+}
